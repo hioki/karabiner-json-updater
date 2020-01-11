ae1f9b003e5b750e223ab710be993c1ac053fc06
Daichi Hioki <hioki.daichi@gmail.com>
2020-01-12 00:03:59 +0900

    Use enum: Condition

--- src/condition.rs
+++ src/condition.rs
@@ -4,35 +4,32 @@ use crate::virtual_key::VirtualKey;
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
-pub struct Condition {
-    pub r#type: ConditionType,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub bundle_identifiers: Option<Vec<BundleIdentifier>>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<VirtualKey>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub value: Option<u8>,
+#[serde(untagged)]
+pub enum Condition {
+    OnApplication {
+        r#type: ConditionType,
+        bundle_identifiers: Vec<BundleIdentifier>,
+    },
+    WithVirtualKey {
+        r#type: ConditionType,
+        name: VirtualKey,
+        value: u8,
+    },
 }
 
 impl Condition {
     pub fn on_app(bundle_identifier: BundleIdentifier) -> Condition {
-        Condition {
+        Condition::OnApplication {
             r#type: ConditionType::FrontmostApplicationIf,
-            bundle_identifiers: Some(vec![bundle_identifier]),
-            name: None,
-            value: None,
+            bundle_identifiers: vec![bundle_identifier],
         }
     }
 
     pub fn with_virtual_key(virtual_key: VirtualKey) -> Condition {
-        Condition {
+        Condition::WithVirtualKey {
             r#type: ConditionType::VariableIf,
-            bundle_identifiers: None,
-            name: Some(virtual_key),
-            value: Some(Value::On.value()),
+            name: virtual_key,
+            value: Value::On.value(),
         }
     }
 }
