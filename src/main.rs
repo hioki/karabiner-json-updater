6b4a6685c3e86a413bc1219e450773b2a269d3c1
Daichi Hioki <hioki.daichi@gmail.com>
2020-01-13 23:21:51 +0900

    Update karabiner.json with Rust instead of Bash

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 mod config;
+mod updater;
 
 use crate::config::config::Config;
+use crate::updater::updater::Updater;
+use std::process;
 
 fn main() {
-    println!("{}", Config::my_config().to_json());
+    let config = Config::my_config();
+    let updater = Updater { config };
+
+    process::exit(match updater.update() {
+        Ok(_) => {
+            println!("done.");
+            0
+        }
+        Err(message) => {
+            eprintln!("{}", message);
+            1
+        }
+    })
 }
---
+++ src/updater/updater.rs
@@ -0,0 +1,60 @@
+use crate::config::config::Config;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+pub struct Updater {
+    pub config: Config,
+}
+
+impl Updater {
+    pub fn update(&self) -> Result<(), String> {
+        let is_jq_installed = Command::new("type")
+            .arg("jq")
+            .stdout(Stdio::null())
+            .status()
+            .unwrap()
+            .success();
+
+        if !is_jq_installed {
+            return Err("jq must be installed".to_owned());
+        }
+
+        let config_dir = concat!(env!("HOME"), "/.config/karabiner");
+
+        let config_dir_exists = Path::new(config_dir).is_dir();
+
+        if !config_dir_exists {
+            return Err("Create `$HOME/.config/karabiner/` via Karabiner-Elements".to_owned());
+        }
+
+        let personal_rule_path = format!(
+            "{}/assets/complex_modifications/personal_rules.json",
+            config_dir,
+        );
+
+        let personal_rule_file = File::create(personal_rule_path).unwrap();
+
+        serde_json::to_writer_pretty(personal_rule_file, &self.config).unwrap();
+
+        let karabiner_json_path = format!("{}/karabiner.json", config_dir);
+
+        let rules_json = serde_json::to_string_pretty(&self.config.rules).unwrap();
+
+        let query = format!(".profiles[0].complex_modifications.rules = {}", rules_json);
+
+        let new_json = Command::new("jq")
+            .arg(query)
+            .arg(&karabiner_json_path)
+            .output()
+            .unwrap()
+            .stdout;
+
+        let mut karabiner_json_file = File::create(&karabiner_json_path).unwrap();
+
+        let _size = karabiner_json_file.write(&new_json).unwrap();
+
+        Ok(())
+    }
+}
