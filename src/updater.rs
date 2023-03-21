a47e86d8c0e2505bd477d862c83a9049f4b94ff1
Daichi Hioki <hioki.daichi@gmail.com>
2023-03-21 17:09:48 +0900

    remove: jq dependency

--- src/updater.rs
+++ src/updater.rs
@@ -1,10 +1,10 @@
 use anyhow::Result;
 use serde::Serialize;
+use serde_json::{json, Value};
 use std::{
     fs::{File, OpenOptions},
-    io::{copy, Seek as _, SeekFrom, Write as _},
+    io::{copy, Read, Seek as _, SeekFrom, Write as _},
     path::PathBuf,
-    process::Command,
 };
 
 use crate::config::rule::Rule;
@@ -62,16 +62,21 @@ impl<'a> Updater<'a> {
         let mut custom_file_in_config = File::create(&self.custom_path)?;
         copy(&mut custom_file_in_current_dir, &mut custom_file_in_config)?;
 
-        // Update $HOME/.config/karabiner.json
-        // TODO: remove jq dependency
-        let new_json = Command::new("jq")
-            .arg(format!(
-                ".profiles[0].complex_modifications.rules = {}",
-                serde_json::to_string_pretty(&self.rules)?
-            ))
-            .arg(&self.karabiner_json_path)
-            .output()?
-            .stdout;
+        // Update $HOME/.config/karabiner/karabiner.json
+        let mut f = File::open(&self.karabiner_json_path)?;
+        let mut s = String::new();
+        f.read_to_string(&mut s)?;
+        let mut config: Value = serde_json::from_str(&s).unwrap();
+        if let Some(profiles) = config["profiles"].as_array_mut() {
+            if let Some(profile) = profiles.get_mut(0) {
+                if let Some(complex_modifications) =
+                    profile["complex_modifications"].as_object_mut()
+                {
+                    complex_modifications.insert("rules".to_string(), json!(&self.rules));
+                }
+            }
+        }
+        let new_json = serde_json::to_vec_pretty(&config).unwrap();
         let mut karabiner_json_file = File::create(&self.karabiner_json_path)?;
         let _written = karabiner_json_file.write(&new_json)?;
 
