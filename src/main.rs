4ce1646a2d9c437f37722c4e99122ed1336cb0a0
Daichi Hioki <hioki.daichi@gmail.com>
2025-02-22 22:07:36 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,19 @@ mod config;
 mod config_updater;
 mod custom_rules_builder;
 
-use anyhow::{anyhow, Result};
-use std::path::{Path, PathBuf};
-
 use crate::{config_updater::ConfigUpdater, custom_rules_builder::CustomRulesBuilder};
 
-fn main() -> Result<()> {
-    let config_dir = get_config_dir()?;
+fn main() -> anyhow::Result<()> {
+    // https://karabiner-elements.pqrs.org/docs/json/location/
+    let config_dir = std::env::var("HOME")
+        .map(std::path::PathBuf::from)
+        .expect("HOME environment variable must be set")
+        .join(".config/karabiner");
+    if !config_dir.is_dir() {
+        anyhow::bail!("{:?} must be created via Karabiner-Elements", config_dir);
+    }
     let custom_rules_builder = CustomRulesBuilder::new();
     let custom_rules = custom_rules_builder.build();
     let config_updater = ConfigUpdater::new(config_dir, custom_rules);
     config_updater.update()
-}
-
-// https://karabiner-elements.pqrs.org/docs/json/location/
-fn get_config_dir() -> Result<PathBuf> {
-    let config_dir = Path::new(env!("HOME")).join(".config/karabiner");
-    if !config_dir.is_dir() {
-        return Err(anyhow!(format!(
-            "{} must be created via Karabiner-Elements",
-            config_dir.to_str().unwrap()
-        )));
-    }
-    Ok(config_dir)
 }
